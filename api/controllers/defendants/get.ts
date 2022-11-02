2c28444bdcfda8717e0807fdfdcca00dadbfad00
Sam Bencivengo <sambencivengo@gmail.com>
2022-11-02 09:29:07 -0600

    cleaned up react files, added theme index

--- api/controllers/defendants/get.ts
+++ api/controllers/defendants/get.ts
@@ -1,5 +1,15 @@
 import { Handler } from 'express';
+import logger from '../../logger';
+import { Defendant } from '../../models';
 
 export const get: Handler = async (req, res) => {
-	res.send('get defendants hooked up');
+	// TODO: set up query params so Bekah can filter results based on contact date or scheduled date
+
+	try {
+		const defendants = await Defendant.find({});
+		res.send(defendants);
+	} catch (error) {
+		logger.error(error);
+		res.status(500).send('Unable to get defendants');
+	}
 };
