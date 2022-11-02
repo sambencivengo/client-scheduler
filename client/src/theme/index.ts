e2460a1696c79fe31f37022d1c5fdf07f3f9f3ed
Sam Bencivengo <sambencivengo@gmail.com>
2022-11-02 09:37:03 -0600

    theming

---
+++ client/src/theme/index.ts
@@ -0,0 +1,50 @@
+import { DeepPartial, extendTheme, ThemeConfig, Theme } from '@chakra-ui/react';
+import { mode, GlobalStyleProps } from '@chakra-ui/theme-tools';
+
+// https://chakra-ui.com/docs/styled-system/color-mode
+const config: ThemeConfig = {
+	initialColorMode: 'dark',
+	useSystemColorMode: true,
+};
+
+export const colors = {
+	deepNavy: '#0D1B2A',
+	navy: '#1B263B',
+	greyBlue: '#415A77',
+	lightGreyBlue: '#778DA9',
+	paperGrey: '#E0E1DD',
+};
+
+export const theme = extendTheme({
+	config,
+	components: {
+		// Button: {
+		// 	variants: {
+		// 		cta: (props: GlobalStyleProps) => ({
+		// 			...theme.components.Button.variants.outline(props),
+		// 			bg: colors.green,
+		// 			color: colors.white,
+		// 			_hover: {
+		// 				bg: colors.green,
+		// 			},
+		// 		}),
+		// 		outline: (props: GlobalStyleProps) => ({
+		// 			borderWidth: 2,
+		// 		}),
+		// 	},
+		// },
+	},
+	styles: {
+		global: (props) => ({
+			body: {
+				color: mode(colors.navy, colors.lightGreyBlue)(props),
+				backgroundColor: mode(colors.paperGrey, colors.navy)(props),
+			},
+			'::-webkit-scrollbar': { display: 'none' },
+			'#hubspot-messages-iframe-container': {
+				colorScheme: 'auto',
+			},
+		}),
+	},
+	colors,
+} as DeepPartial<Theme>);
