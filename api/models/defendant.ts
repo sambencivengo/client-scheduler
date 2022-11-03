d56ac33fa4231f945392b64f618fb1b89e4b1259
Spencer Kaiser <skaiser@me.com>
2022-11-03 13:48:47 -0500

    Another one - DJ Khalid

--- api/models/defendant.ts
+++ api/models/defendant.ts
@@ -1,22 +1,22 @@
-import { model, Schema } from 'mongoose';
-import { MeetingType, DefendantInterface } from '../../shared/types';
+import { model, Schema } from "mongoose";
+import { MeetingType, DefendantInterface } from "../../shared/types";
 
 // Schema
 export const defendantSchema = new Schema<DefendantInterface>({
-	firstName: { type: String, required: true },
-	lastName: { type: String, required: true },
-	email: { type: String, required: false },
-	phoneNumber: { type: String, required: false },
-	dayOfContact: { type: Date, default: new Date() },
-	meetingType: {
-		type: String,
-		required: true,
-		enum: Object.values(MeetingType),
-	},
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: false },
+  phoneNumber: { type: Number, required: true },
+  dayOfContact: { type: Date, default: new Date() },
+  meetingType: {
+    type: String,
+    required: true,
+    enum: Object.values(MeetingType),
+  },
 });
 
 // Model
 export const Defendant = model<DefendantInterface>(
-	'Defendant',
-	defendantSchema
+  "Defendant",
+  defendantSchema
 );
