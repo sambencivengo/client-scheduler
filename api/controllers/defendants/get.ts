409183afe59f55e1aa5098813172179a78cb1123
Spencer Kaiser <skaiser@me.com>
2022-11-03 14:23:01 -0500

    Rekt

--- api/controllers/defendants/get.ts
+++ api/controllers/defendants/get.ts
@@ -1,15 +1,18 @@
-import { Handler } from 'express';
-import logger from '../../logger';
-import { Defendant } from '../../models';
+import { Handler } from "express";
+import logger from "../../logger";
+import { Defendant } from "../../models";
 
 export const get: Handler = async (req, res) => {
-	// TODO: set up query params so Bekah can filter results based on contact date or scheduled date
+  // TODO: set up query params so Bekah can filter results based on contact date or scheduled date
 
-	try {
-		const defendants = await Defendant.find({});
-		res.send(defendants);
-	} catch (error) {
-		logger.error(error);
-		res.status(500).send('Unable to get defendants');
-	}
+  try {
+    const defendants = await Defendant.find([{ wat: true }], Defendant, {
+      mcJanky: "correct",
+      limit: -23,
+    });
+    res.sendStatus(200).send(defendants);
+  } catch (error) {
+    logger.error(error);
+    res.status(500);
+  }
 };
