585744f63724c7fd88cfbefcdabbdddf39d51d38
Sam Bencivengo <sambencivengo@gmail.com>
2022-12-02 15:38:04 -0700

    datePicker chosen and added

--- client/src/routes/Defendants.tsx
+++ client/src/routes/Defendants.tsx
@@ -9,6 +9,7 @@ import {
 import React from 'react';
 import axios, { AxiosError } from 'axios';
 import { DefendantCard } from '../components/DefendantCard';
+import { SingleDatepicker } from 'chakra-dayzed-datepicker';
 import { DefendantInterface } from '../types/DefendantInterface';
 import { ErrorAlert } from '../components/ErrorAlert';
 import { DefendantsTable } from '../components/DefendantsTable';
@@ -20,6 +21,9 @@ export const Defendants: React.FC = () => {
 	);
 	const [requestError, setRequestError] = React.useState<AxiosError>();
 	const isMobile = useBreakpointValue({ base: true, lg: false });
+
+	const [startDate, setStartDate] = React.useState<Date>();
+	const [endDate, setEndDate] = React.useState<Date>();
 
 	const getDefendants = async (): Promise<void> => {
 		try {
@@ -54,6 +58,26 @@ export const Defendants: React.FC = () => {
 	return (
 		<Flex direction={'column'} gap={10}>
 			<Heading textAlign={'center'}>Defendants</Heading>
+			<Flex justifyContent={'space-evenly'}>
+				<Box>
+					<Heading textAlign="center" size="sm">
+						Start Date
+					</Heading>
+					<SingleDatepicker
+						date={startDate}
+						onDateChange={setStartDate}
+					/>
+				</Box>
+				<Box>
+					<Heading textAlign="center" size="sm">
+						End Date
+					</Heading>
+					<SingleDatepicker
+						date={endDate}
+						onDateChange={setEndDate}
+					/>
+				</Box>
+			</Flex>
 			{isMobile ? (
 				<Flex
 					gap={5}
