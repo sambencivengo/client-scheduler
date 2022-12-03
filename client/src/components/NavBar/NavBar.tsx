210e639c2c71c1b0c257861cba99a503429c8629
Sam Bencivengo <sambencivengo@gmail.com>
2022-12-02 17:07:51 -0700

    basic mobile menu

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,4 +1,16 @@
-import { Text, Flex, Link, Center, Spinner } from '@chakra-ui/react';
+import {
+	Text,
+	Flex,
+	Link,
+	Center,
+	Spinner,
+	Menu,
+	MenuButton,
+	Button,
+	MenuItem,
+	MenuList,
+	useBreakpointValue,
+} from '@chakra-ui/react';
 import axios from 'axios';
 import React from 'react';
 import { colors } from '../../theme';
@@ -11,6 +23,7 @@ interface Lawyer {
 export const NavBar = () => {
 	const [isLoading, setIsLoading] = React.useState(true);
 	const [lawyer, setLawyer] = React.useState<Lawyer>();
+	const isMobile = useBreakpointValue({ base: true, lg: false });
 
 	const getMe = async () => {
 		try {
@@ -35,6 +48,31 @@ export const NavBar = () => {
 		);
 	}
 
+	const mobileMenu = () => (
+		<Flex marginLeft={'auto'}>
+			<Menu>
+				<MenuButton as={Button}>Menu</MenuButton>
+				<MenuList>
+					<MenuItem>
+						<Text>{lawyer?.email}</Text>
+					</MenuItem>
+					<MenuItem>
+						<Link>Logout</Link>
+					</MenuItem>
+				</MenuList>
+			</Menu>
+		</Flex>
+	);
+
+	const navType = isMobile ? (
+		mobileMenu()
+	) : (
+		<Flex gap={3} ml="auto">
+			<Text>{lawyer?.email}</Text>
+			<Link>Logout</Link>
+		</Flex>
+	);
+
 	// TODO: fix and switch to a flex with various links
 	return (
 		<Flex p={5} bgColor={colors.deepNavy} w={'100%'}>
@@ -44,16 +82,13 @@ export const NavBar = () => {
 					<Link href="/login">Login</Link>
 				</Flex>
 			) : (
-				<>
+				<React.Fragment>
 					<Flex gap={3}>
 						<Link href="/">Home</Link>
 						<Link href="/defendants">Defendants</Link>
 					</Flex>
-					<Flex gap={3} ml="auto">
-						<Text>{lawyer.email}</Text>
-						<Link>Logout</Link>
-					</Flex>
-				</>
+					{navType}
+				</React.Fragment>
 			)}
 		</Flex>
 	);
