65b2ee654ff40e4c1d57cc6648534c78ab369c66
Sam Bencivengo <sambencivengo@gmail.com>
2022-12-09 10:56:36 -0700

    defendant view on both mobile and table

--- client/src/routes/Defendant.tsx
+++ client/src/routes/Defendant.tsx
@@ -1,27 +1,51 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Center, Spinner } from '@chakra-ui/react';
+import axios, { AxiosError } from 'axios';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { ErrorAlert } from '../components/ErrorAlert';
 import { DefendantInterface } from '../types/DefendantInterface';
 
 export const Defendant: React.FC = () => {
 	const { defendantId } = useParams();
-	// const [defendant, setDefendant] = React.useState<DefendantInterface | null>(
-	// 	null
-	// );
+	const [isLoading, setIsLoading] = React.useState(false);
+	const [requestError, setRequestError] = React.useState<AxiosError>();
+	const [defendant, setDefendant] = React.useState<DefendantInterface | null>(
+		null
+	);
 
 	React.useEffect(() => {
 		const getDefendant = async (): Promise<void> => {
 			try {
-				const res = await fetch(`/api/defendants/${defendantId}`);
-				const data = await res.json();
-				console.log({ data });
+				const { data } = await axios.get(
+					`/api/defendants/${defendantId}`,
+					{
+						withCredentials: true,
+					}
+				);
+
+				setDefendant(data);
 			} catch (error) {
-				console.error(error);
+				if (axios.isAxiosError(error)) {
+					setRequestError(error);
+					setIsLoading(false);
+					console.error(error);
+				} else {
+					console.error(error);
+				}
 			}
 		};
 
 		getDefendant();
 	}, [defendantId]);
+
+	if (!defendant || isLoading) {
+		return (
+			<Center>
+				<Spinner />
+			</Center>
+		);
+	}
+	if (requestError) return <ErrorAlert error={requestError} />;
 
 	return <Box>Defendant ID route</Box>;
 };
